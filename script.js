604c477c6f73e0419f9ca4f08a44cc2cc6702ada
yasharmv <yasharshm@code.edu.az>
2020-08-31 15:17:00 +0400

    test2

---
+++ script.js
@@ -0,0 +1,17 @@
+var btn = document.getElementsByClassName("open");
+// var button = document.getElementById("open");
+// var box = document.getElementbyId("box");
+
+// box.onclick = qutunuAc.onclick = function () {
+//   if (box.className == "open") {
+//     box.className = "";
+//     qutunuAc.innerHTML = "QUTUNU AC";
+//     qutunuAc.onclick = "fqg";
+//     qutunuAc.name;
+//   } else {
+//     qutunuAc.innerHTML = "qutunu bagla";
+//     box.className = "openedBox";
+//   }
+// };
+
+console.log(btn.map);
